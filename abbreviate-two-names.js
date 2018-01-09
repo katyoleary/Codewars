2d710fe8b169391ca9526b21d57980dae8149271
Katy Robinson <katyorob@gmail.com>
2018-01-09 09:25:22 -0800

    abbreviate names codewars challenge complete

---
+++ abbreviate-two-names.js
@@ -0,0 +1,15 @@
+//my code//
+
+function abbrevName(name){
+    var separate = name.split(' ').map(function(name){return name[0]}).join('.');
+    return separate;
+
+}
+
+//sample tests
+
+Test.assertEquals(abbrevName("Sam Harris"), "S.H");
+Test.assertEquals(abbrevName("Patrick Feenan"), "P.F");
+Test.assertEquals(abbrevName("Evan Cole"), "E.C");
+Test.assertEquals(abbrevName("P Favuzzi"), "P.F");
+Test.assertEquals(abbrevName("David Mendieta"), "D.M");
