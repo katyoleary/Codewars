db79dedb5bb6d35650b905efef2386416f75b889
Katy Robinson <katyorob@gmail.com>
2018-01-25 19:32:29 -0800

    made prettier

--- disemvowel-trolls/solution.js
+++ disemvowel-trolls/solution.js
@@ -3,7 +3,7 @@
 function disemvowel(str) {
   var vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
   var solution = "";
-  for (i=0; i<=str.length; i++) {
+  for (i = 0; i < str.length; i++) {
     if (vowels.indexOf(str.charAt(i)) === -1) {
       solution = solution + str.charAt(i);
     }
